3f4e8165e4e81835525403a45a4537c633e97e5a
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2018-09-28 15:14:39 +0900

    Add files via upload

--- test3/main.js
+++ test3/main.js
@@ -3,28 +3,38 @@
       window.onload = init;
 
       function init() {
-            console.log('OK');
-
-            let time = {
-                  start: null,
-                  count: null
-            };
-
-            let define = {
-                  sat: 0.4,
-                  check: 1.0,
-                  l_size: 0.05
-            };
-
-            let rgb_balance = {
-                  red: 0.0,
-                  green: 0.0,
-                  blue: 1.0
-            };
+            console.log('[OK]onload');
 
             let canvas = document.getElementById('webgl-canvas');
             let height = 512;
             let width = 512;
+
+            document.body.style.zoom = 0.75;
+            canvas.addEventListener('mousemove', mousemove);
+            document.getElementsByTagName('button')[0].addEventListener('click', submit);
+
+            let time = {
+                start: null,
+                count: null
+            };
+
+            let mouse = {
+                x: null,
+                y: null
+            };
+
+            let define = {
+                sat: 0.6,
+                check: 1.0,
+                light_size: 0.05,
+                box_size: 16.0
+            };
+
+            let rgb_balance = {
+                red: 0.0,
+                green: 0.0,
+                blue: 1.0
+            };
 
             canvas.height = height;
             canvas.width = width;
@@ -33,23 +43,25 @@
             let program = create_program(create_shader('vs'), create_shader('fs'));
 
             let uniform = [];
-            uniform[0] = gl_ctx.getUniformLocation(program, 't'); // time
-            uniform[1] = gl_ctx.getUniformLocation(program, 'r'); // resolution
+            uniform[0] = gl_ctx.getUniformLocation(program, 'time');
+            uniform[1] = gl_ctx.getUniformLocation(program, 'resolution');
+            uniform[2] = gl_ctx.getUniformLocation(program, 'mouse');
 
             /*
                   #define sat 0.4
                   #define check 1.0
-                  #define l_size 0.05
+                  #define light_size 0.05
             */
 
-            uniform[2] = gl_ctx.getUniformLocation(program, 'sat');
-            uniform[3] = gl_ctx.getUniformLocation(program, 'check');
-            uniform[4] = gl_ctx.getUniformLocation(program, 'l_size');
+            uniform[3] = gl_ctx.getUniformLocation(program, 'sat');
+            uniform[4] = gl_ctx.getUniformLocation(program, 'check');
+            uniform[5] = gl_ctx.getUniformLocation(program, 'light_size');
+            uniform[6] = gl_ctx.getUniformLocation(program, 'box_size');
 
             // RGB balance
-            uniform[5] = gl_ctx.getUniformLocation(program, 'red');
-            uniform[6] = gl_ctx.getUniformLocation(program, 'green');
-            uniform[7] = gl_ctx.getUniformLocation(program, 'blue');
+            uniform[7] = gl_ctx.getUniformLocation(program, 'red');
+            uniform[8] = gl_ctx.getUniformLocation(program, 'green');
+            uniform[9] = gl_ctx.getUniformLocation(program, 'blue');
 
             let position = [
                   -1.0,  1.0,  0.0,
@@ -73,30 +85,43 @@
             gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, vbo_Index);
             gl_ctx.clearColor(0.0, 0.0, 0.0, 1.0);
 
-            time.start = new Date().getTime();
-            time.count = 0;
             tick();
 
             function tick() {
-                  time.count ++;
-                  let time_stamp = ((time.start + time.count) - time.start) * 0.02;
-                  gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT);
+                if (time.start === null) {
+                    time.start = new Date().getTime();
+                    time.count = 0;
 
-                  gl_ctx.uniform1f(uniform[0], time_stamp + 0.0);
-                  gl_ctx.uniform2fv(uniform[1], [width, height]);
+                    console.log('[OK]tick');
+                }
 
-                  gl_ctx.uniform1f(uniform[2], define.sat);
-                  gl_ctx.uniform1f(uniform[3], define.check);
-                  gl_ctx.uniform1f(uniform[4], define.l_size);
+                // color from html input
+                colors = Array.from(document.getElementsByClassName('colors')).map(dom => dom.value);
+                rgb_balance.red = colors[0];
+                rgb_balance.green = colors[1];
+                rgb_balance.blue = colors[2];
 
-                  gl_ctx.uniform1f(uniform[5], rgb_balance.red);
-                  gl_ctx.uniform1f(uniform[6], rgb_balance.green);
-                  gl_ctx.uniform1f(uniform[7], rgb_balance.blue);
+                time.count ++;
+                let time_stamp = ((time.start + time.count) - time.start) * 0.02;
+                gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT);
 
-                  gl_ctx.drawElements(gl_ctx.TRIANGLES, 6, gl_ctx.UNSIGNED_SHORT, 0);
-                  gl_ctx.flush();
+                gl_ctx.uniform1f(uniform[0], time_stamp + 0.0);
+                gl_ctx.uniform2fv(uniform[1], [width, height]);
+                gl_ctx.uniform2fv(uniform[2], [mouse.x, mouse.y]);
 
-                  requestAnimationFrame(tick);
+                gl_ctx.uniform1f(uniform[3], define.sat);
+                gl_ctx.uniform1f(uniform[4], define.check);
+                gl_ctx.uniform1f(uniform[5], define.light_size);
+                gl_ctx.uniform1f(uniform[6], define.box_size);
+
+                gl_ctx.uniform1f(uniform[7], rgb_balance.red);
+                gl_ctx.uniform1f(uniform[8], rgb_balance.green);
+                gl_ctx.uniform1f(uniform[9], rgb_balance.blue);
+
+                gl_ctx.drawElements(gl_ctx.TRIANGLES, 6, gl_ctx.UNSIGNED_SHORT, 0);
+                gl_ctx.flush();
+
+                requestAnimationFrame(tick);
             }
 
             function create_program(vs, fs) {
@@ -157,13 +182,29 @@
                   return ibo;
             }
 
-            // submit status
+            // custom functions
             function submit() {
-                  let input = document.getElementsByTagName('input');
-                  define.sat = parseFloat(input[0].value, 10) || 0.4;
-                  define.l_size = parseFloat(input[1].value, 10) || 0.05;
+                  let input = Array.from(document.getElementsByClassName('input-box')).map(dom => isNaN(dom.value) || dom.value < 0.0 ? 0.0 : Number(dom.value));
+
+                  define.sat = input[0];
+                  define.light_size = input[1];
+                  define.box_size = input[2];
             }
 
-            (document.getElementsByTagName('button')[0]).addEventListener('click', () => submit());
+            function mousemove(event) {
+                let rect = event.target.getBoundingClientRect();
+                let x = event.clientX - rect.left;
+                let y = event.clientY - rect.top;
+
+/*
+                mouse.x = x;//(x - 256) / 256;
+                mouse.y = -(y - 180);
+*/
+
+                console.log(mouse.x, mouse.y);
+
+                //mouse.x = x - 256;
+                //mouse.y = -(y - 256);
+            }
       }
 })();
